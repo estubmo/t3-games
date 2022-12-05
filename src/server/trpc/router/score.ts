47e8e2598904832db81a0ebd93a3b059669fcb41
Eirik Mo <eirik@mowebdev.com>
2022-12-05 19:57:07 +0100

    Add some new functions

--- src/server/trpc/router/score.ts
+++ src/server/trpc/router/score.ts
@@ -25,4 +25,29 @@ export const scoreRouter = router({
         where: { gameId },
       });
     }),
+  topTenByGameId: publicProcedure
+    .input(
+      z.object({
+        gameId: z.string(),
+      })
+    )
+    .query(({ ctx, input }) => {
+      const { gameId } = input;
+      return ctx.prisma.score.findMany({
+        where: { gameId },
+        orderBy: { score: "desc" },
+        take: 10,
+      });
+    }),
+  addScoreByGameId: publicProcedure
+    .input(
+      z.object({ score: z.number(), name: z.string(), gameId: z.string() })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.score.create({
+        data: {
+          ...input,
+        },
+      });
+    }),
 });
