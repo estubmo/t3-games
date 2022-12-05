0248814599a06e69563991fbb4bb401a4809cd10
Eirik Mo <eirik@mowebdev.com>
2022-12-05 19:57:55 +0100

    Wrapper improvements

--- src/pages/tins.tsx
+++ src/pages/tins.tsx
@@ -1,35 +1,36 @@
 import TINSGame from "@games/TINSGame";
-import React, { useEffect, useRef, useState } from "react";
+import { trpc } from "@utils/trpc";
+import React, { useLayoutEffect, useRef, useState } from "react";
 
 const MAX_WIDTH = 1920;
 const MAX_HEIGHT = 1080;
 
+type WindowDimensions = {
+  width: number;
+  height: number;
+};
+
 const TINS = () => {
   const divRef = useRef<HTMLDivElement>(null);
-
-  type WindowDimensions = {
-    width: number;
-    height: number;
-  };
   const [dimension, setDimensions] = useState<WindowDimensions>({
     width: 0,
     height: 0,
   });
 
-  useEffect(() => {
-    if (typeof divRef.current !== undefined && divRef.current) {
-      setDimensions({
-        width:
-          divRef.current.clientWidth > MAX_WIDTH
-            ? MAX_WIDTH
-            : divRef.current.clientWidth,
-        height:
-          divRef.current.clientHeight > MAX_HEIGHT
-            ? MAX_HEIGHT
-            : divRef.current.clientHeight,
-      });
-    }
-  }, []);
+  useLayoutEffect(() => {
+    if (!divRef.current) return;
+    const newDimensions = {
+      width:
+        divRef.current.clientWidth > MAX_WIDTH
+          ? MAX_WIDTH
+          : divRef.current.clientWidth,
+      height:
+        divRef.current.clientHeight > MAX_HEIGHT
+          ? MAX_HEIGHT
+          : divRef.current.clientHeight,
+    };
+    setDimensions(newDimensions);
+  }, [divRef]);
 
   return (
     <div
